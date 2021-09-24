f6a1f2e7bc644f1721be3638921885f403d1f011
Andrés Milla <andressmilla@gmail.com>
2021-09-24 13:30:22 -0300

    Added ED solution

---
+++ src/edit_distance/main.py
@@ -0,0 +1,50 @@
+"""
+Python version: 3.7.3
+
+https://www.spoj.com/problems/EDIST/
+
+Note: Invalid submission because the judge's Python3 is broken:
+https://www.spoj.com/ranks/EDIST/lang=PYTH%203.2.3
+"""
+
+from sys import stdin, stdout
+
+
+def main():
+    total_cases = int(stdin.readline())
+
+    for _ in range(total_cases):
+        word_a, word_b = (stdin.readline().strip() for _ in range(2))
+
+        minimal_operations = edit_distance(word_a, word_b)
+
+        stdout.write(f"{minimal_operations}\n")
+
+
+def edit_distance(word_a, word_b):
+    if word_a == word_b:
+        return 0
+
+    if len(word_a) == 0:
+        return len(word_b)
+
+    if len(word_b) == 0:
+        return len(word_a)
+
+    N, M = len(word_a) + 1, len(word_b) + 1
+
+    COSTS = [list(range(M))] + [[i] for i in range(1, N)]
+
+    for i in range(1, N):
+        for j in range(1, M):
+            replace = COSTS[i - 1][j - 1] + (word_a[i - 1] != word_b[i - 1])
+            delete = COSTS[i - 1][j] + 1
+            insert = COSTS[i][j - 1] + 1
+
+            COSTS[i].append(min(replace, delete, insert))
+
+    return COSTS[N - 1][M - 1]
+
+
+if __name__ == "__main__":
+    main()
